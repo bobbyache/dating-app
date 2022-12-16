076b4ab4954312431d0922bc3fe0d8e51ea40a8b
RobBl8ke <RobB@korbicom.com>
2022-12-16 06:28:18 +0200

    Problem with no such table __EFMigrationsHistory

--- API/Program.cs
+++ API/Program.cs
@@ -1,8 +1,20 @@
+using API.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+// https://github.com/trycatchlearn/datingapp
+
 builder.Services.AddControllers();
+
+// Add DB Context, set up the default connection, and use Sql Lite.
+builder.Services.AddDbContext<DataContext>(options => 
+{
+    options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
