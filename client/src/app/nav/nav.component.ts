de5d069f3ac03e8954206c93da282222fb8ae35b
RobBl8ke <RobB@korbicom.com>
2022-12-21 08:03:42 +0200

    Remove subscribe in favour of async pipe for tracking user logged in status

--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable, of } from 'rxjs';
+import { User } from '../_models/user';
 import { AccountsService } from '../_services/accounts.service';
 
 @Component({
@@ -8,26 +10,18 @@ import { AccountsService } from '../_services/accounts.service';
 })
 export class NavComponent implements OnInit {
     model: any = {};
-    loggedIn = false;
+    currentUser$: Observable<User | null> = of(null);
 
     constructor(private accountService: AccountsService) {}
 
     ngOnInit(): void {
-        this.getCurrentUser();
-    }
-
-    getCurrentUser() {
-        this.accountService.currentUser$.subscribe({
-            next: user => this.loggedIn = !!user,
-            error: error => console.log(error)
-        });
+        this.currentUser$ = this.accountService.currentUser$;
     }
 
     login() {
         this.accountService.login(this.model).subscribe({
             next: response => {
                 console.log(response);
-                this.loggedIn = true;
             },
             error: error => console.log(error)
         });
@@ -35,6 +29,5 @@ export class NavComponent implements OnInit {
 
     logout() {
         this.accountService.logout();
-        this.loggedIn = false;
     }
 }
