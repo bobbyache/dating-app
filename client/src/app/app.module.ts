f9962afd050c56be298b4c529550a9e7c54c1d7a
RobBl8ke <RobB@korbicom.com>
2022-12-22 18:12:11 +0200

    Creating a shared module

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -8,14 +8,13 @@ import { NavComponent } from './nav/nav.component';
 import { FormsModule } from '@angular/forms';
 
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 import { HomeComponent } from './home/home.component';
 import { RegisterComponent } from './register/register.component';
 import { MemberListComponent } from './members/member-list/member-list.component';
 import { MemberDetailComponent } from './members/member-detail/member-detail.component';
 import { ListsComponent } from './lists/lists.component';
 import { MessagesComponent } from './messages/messages.component';
-import { ToastrModule } from 'ngx-toastr';
+import { SharedModule } from './_modules/shared.module';
 
 @NgModule({
     declarations: [AppComponent, NavComponent, HomeComponent, RegisterComponent, MemberListComponent, MemberDetailComponent, ListsComponent, MessagesComponent],
@@ -24,11 +23,8 @@ import { ToastrModule } from 'ngx-toastr';
         AppRoutingModule,
         HttpClientModule,
         FormsModule,
-        BrowserAnimationsModule,
-        BsDropdownModule.forRoot(),
-        ToastrModule.forRoot({
-            positionClass: 'toast-bottom-right'
-        }),
+        SharedModule,
+        BrowserAnimationsModule
     ],
     providers: [],
     bootstrap: [AppComponent],
