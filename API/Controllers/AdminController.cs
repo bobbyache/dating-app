9f0e2651ad57e6407156ffbdec2b36ae1b659192
Rob Blake <rob.bl8ke@outlook.com>
2024-07-27 07:44:27 +0200

    Get and edit user roles (API)

--- API/Controllers/AdminController.cs
+++ API/Controllers/AdminController.cs
@@ -1,15 +1,57 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using API.Entities;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers;
 
 public class AdminController : BaseApiController
 {
+    private readonly UserManager<AppUser> userManager;
+
+    public AdminController(UserManager<AppUser> userManager)
+    {
+        this.userManager = userManager;
+    }
     [Authorize(Policy = "RequireAdminRole")]
     [HttpGet("users-with-roles")]
-    public ActionResult GetUsersWithRoles()
+    public async Task<ActionResult> GetUsersWithRoles()
     {
-        return Ok("Only admins can see this");
+        var users = await userManager.Users
+            .OrderBy(u => u.UserName)
+            .Select(u => new {
+                u.Id,
+                Username = u.UserName,
+                Roles = u.UserRoles.Select(r => r.Role.Name).ToList()
+            })
+            .ToListAsync();
+
+        return Ok(users);
+    }
+
+    // Technically, this should be an HTTP PUT (said in the words of the instructor)
+    [Authorize(Policy = "RequireAdminRole")]
+    [HttpPost("edit-roles/{username}")]
+    public async Task<ActionResult> EditRoles(string username, [FromQuery]string roles)
+    {
+        if (string.IsNullOrEmpty(roles)) return BadRequest("You must select at least one role");
+        var selectedRoles = roles.Split(",").ToArray();
+
+        var user = await userManager.FindByNameAsync(username);
+        if (user == null) return NotFound();
+
+        // Add new roles
+        var userRoles = await userManager.GetRolesAsync(user);
+        var result = await userManager.AddToRolesAsync(user, selectedRoles.Except(userRoles));
+
+        if (!result.Succeeded) return BadRequest("Failed to add roles");
+
+        // Remove roles that exist (but are not in the passed in set)
+        result = await userManager.RemoveFromRolesAsync(user, userRoles.Except(selectedRoles));
+
+        if (!result.Succeeded) return BadRequest("Failed to remove roles");
+        return Ok(await userManager.GetRolesAsync(user));
     }
 
     [Authorize(Policy = "ModeratePhotoRole")]
