ea6fb95e2db0605668550f419c55c61fc0ecb547
Rob Blake <rob.bl8ke@outlook.com>
2024-07-29 17:02:16 +0200

    All unit tests passing

--- client/src/app/app.component.spec.ts
+++ client/src/app/app.component.spec.ts
@@ -1,13 +1,21 @@
 import { TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+import { HttpClient } from '@angular/common/http';
 
 describe('AppComponent', () => {
+    let httpClient: HttpClient;
+    let httpTestingController: HttpTestingController;
+    
     beforeEach(async () => {
         await TestBed.configureTestingModule({
-            imports: [RouterTestingModule],
-            declarations: [AppComponent],
+            imports: [RouterTestingModule, HttpClientTestingModule ],
+            declarations: [AppComponent]
         }).compileComponents();
+
+        httpClient = TestBed.inject(HttpClient);
+        httpTestingController = TestBed.inject(HttpTestingController);
     });
 
     it('should create the app', () => {
@@ -16,16 +24,9 @@ describe('AppComponent', () => {
         expect(app).toBeTruthy();
     });
 
-    it(`should have as title 'client'`, () => {
+    it(`should display the correct title`, () => {
         const fixture = TestBed.createComponent(AppComponent);
         const app = fixture.componentInstance;
-        expect(app.title).toEqual('client');
-    });
-
-    it('should render title', () => {
-        const fixture = TestBed.createComponent(AppComponent);
-        fixture.detectChanges();
-        const compiled = fixture.nativeElement as HTMLElement;
-        expect(compiled.querySelector('.content span')?.textContent).toContain('client app is running!');
+        expect(app.title).toEqual('Dating App');
     });
 });
