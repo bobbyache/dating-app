9ce0f36c4333930f7a370e75bd7397133bb23288
Rob Blake <rob.bl8ke@outlook.com>
2024-08-02 07:56:39 +0200

    NavComponent: Testing login and logout (one test remains a problem)

--- client/src/app/nav/nav.component.spec.ts
+++ client/src/app/nav/nav.component.spec.ts
@@ -4,8 +4,10 @@ import { DebugElement } from "@angular/core";
 import { HttpClientModule } from "@angular/common/http";
 import { AccountsService } from "../_services/accounts.service";
 import { Router } from "@angular/router";
+import { of } from "rxjs";
+// import { FormsModule } from "@angular/forms";
 
-fdescribe('NavComponent', () => {
+describe('NavComponent', () => {
     let component: NavComponent;
     let fixture: ComponentFixture<NavComponent>;
     let el: DebugElement;
@@ -19,6 +21,7 @@ fdescribe('NavComponent', () => {
 
         TestBed.configureTestingModule({
             imports: [
+                // FormsModule,
                 HttpClientModule
             ],
             declarations: [
@@ -41,5 +44,50 @@ fdescribe('NavComponent', () => {
         expect(component).toBeTruthy();
         console.log('hello world');
         console.log(component);
+    });
+
+    describe('when logging in', () => {
+        let loginSpy: any;
+
+        beforeEach(() => {
+            loginSpy = (accountServiceStub as jasmine.SpyObj<AccountsService>)
+                .login.and.returnValue(of());
+            // will fail if FormsModule is removed because of ngModel
+            // fixture.detectChanges();
+            component.login();
+        });
+
+        it('should log in through the account service', () => {
+            expect(loginSpy).toHaveBeenCalledTimes(1);
+        });
+        
+        it('should navigate to the members area', () => {
+            pending();
+            // Can't seem to get this right yet... wrapped observable makes this tricky...
+            // (accountServiceStub as any).login().subscribe(() => {
+            // });
+            // expect(routerStub.navigateByUrl).toHaveBeenCalledOnceWith('/members');
+        });
+    });
+
+    describe('when logging out', () => {
+        let logoutSpy: any;
+
+        beforeEach(() => {
+            logoutSpy = (accountServiceStub as jasmine.SpyObj<AccountsService>)
+                .logout;
+            // fixture.detectChanges();
+            component.logout();
+        });
+
+        it('should navigate to the landing page', () => {
+            // pending();
+            expect(routerStub.navigateByUrl).toHaveBeenCalledOnceWith('/');
+        });
+
+        it('should log out through the account service', () => {
+            // pending();
+            expect(logoutSpy).toHaveBeenCalledTimes(1);
+        });
     });
 });
