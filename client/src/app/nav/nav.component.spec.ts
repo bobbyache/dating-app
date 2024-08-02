8d342298c7f0e6a7b09f187ed64d668e2f8da889
Rob Blake <rob.bl8ke@outlook.com>
2024-08-02 05:52:46 +0200

    Build up the NavComponent TestBed from scratch

--- client/src/app/nav/nav.component.spec.ts
+++ client/src/app/nav/nav.component.spec.ts
@@ -3,6 +3,10 @@ import { NavComponent } from "./nav.component";
 import { AppModule } from "../app.module";
 import { NoopAnimationsModule } from "@angular/platform-browser/animations";
 import { DebugElement } from "@angular/core";
+import { HttpClientModule } from "@angular/common/http";
+import { ToastrModule, ToastrService } from "ngx-toastr";
+import { CommonModule } from "@angular/common";
+import { BrowserModule } from "@angular/platform-browser";
 
 fdescribe('NavComponent', () => {
     let component: NavComponent;
@@ -11,7 +15,15 @@ fdescribe('NavComponent', () => {
 
     beforeEach(waitForAsync(() => {
         TestBed.configureTestingModule({
-            imports: [AppModule, NoopAnimationsModule],
+            imports: [
+                HttpClientModule,
+                ToastrModule.forRoot({
+                    positionClass: 'toast-bottom-right',
+                })
+            ],
+            declarations: [
+                NavComponent
+            ]
         })
         .compileComponents()
         .then(() => {
