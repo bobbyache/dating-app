2e822ef041d7aa5a4ea504f38ee48b6375dd829c
Rob Blake <rob.bl8ke@outlook.com>
2024-08-02 08:02:02 +0200

    AppComponent: Fix up some missing declarations and imports

--- client/src/app/app.component.spec.ts
+++ client/src/app/app.component.spec.ts
@@ -3,6 +3,8 @@ import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
 import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 import { HttpClient } from '@angular/common/http';
+import { NgxSpinnerModule } from 'ngx-spinner';
+import { NavComponent } from './nav/nav.component';
 
 describe('AppComponent', () => {
     let httpClient: HttpClient;
@@ -10,8 +12,12 @@ describe('AppComponent', () => {
     
     beforeEach(async () => {
         await TestBed.configureTestingModule({
-            imports: [RouterTestingModule, HttpClientTestingModule ],
-            declarations: [AppComponent]
+            imports: [
+                RouterTestingModule, 
+                HttpClientTestingModule, 
+                NgxSpinnerModule.forRoot({ type: 'line-scale-party' }), 
+            ],
+            declarations: [AppComponent, NavComponent]
         }).compileComponents();
 
         httpClient = TestBed.inject(HttpClient);
