11cdb15217a42ce6a15e77335f3fdebd2448fce0
Rob Blake <rob.bl8ke@outlook.com>
2024-08-03 18:33:18 +0200

    Add a backend key/value store to track online users (not scalable - but good prototype starting point)

--- API/SignalR/PresenceHub.cs
+++ API/SignalR/PresenceHub.cs
@@ -5,15 +5,29 @@ namespace API.SignalR;
 
 public class PresenceHub : Hub
 {
+    private readonly PresenceTracker tracker;
+
+    public PresenceHub(PresenceTracker tracker)
+    {
+        this.tracker = tracker;
+    }
+
     public override async Task OnConnectedAsync()
     {
-
+        await tracker.UserConnected(Context.User.GetUsername(), Context.ConnectionId);
         await Clients.Others.SendAsync("UserIsOnline", Context.User.GetUsername());
+
+        var currentUsers = await tracker.GetOnlineUsers();
+        await Clients.All.SendAsync("GetOnlineUsers", currentUsers);
     }
 
     public override async Task OnDisconnectedAsync(Exception exception)
     {
+        await tracker.UserDisconnected(Context.User.GetUsername(), Context.ConnectionId);
         await Clients.Others.SendAsync("UserIsOffline", Context.User.GetUsername());
+        
+        var currentUsers = await tracker.GetOnlineUsers();
+        await Clients.All.SendAsync("GetOnlineUsers", currentUsers);
         
         // Since we're passing through an exception, we also need to call the base method.
         await base.OnDisconnectedAsync(exception);
---
+++ API/SignalR/PresenceTracker.cs
@@ -0,0 +1,64 @@
+﻿using System.Reflection;
+
+namespace API.SignalR;
+
+public class PresenceTracker
+{
+    // Note: This solution does not scale well. However, it's a good prototypical example of the 
+    // idea behind  tracking the history of user's online presence. A better place to store this
+    // may be in a key/value datastore that supports scaling and consistency levels such as
+    // CosmosDB.
+
+    // Users can connect from more than one device (at the same time). Only interested in whether
+    // they're connected to one or more devices we consider them to be online.
+    private static readonly Dictionary<string, List<string>> onlineUsers = new Dictionary<string, List<string>>();
+
+    public Task UserConnected(string username, string connectionId)
+    {
+        // A dictionary is not a thread-safe contruct.  Multiple concurrent users accessing the
+        // dictionary at the same time could cause issues, so use a lock. Not very scalable but
+        // good enough for a prototype in the absence of a more scalable service. It could become
+        // a bottleneck.
+        lock (onlineUsers)
+        {
+            if (onlineUsers.ContainsKey(username))
+            {
+                onlineUsers[username].Add(connectionId);
+            }
+            else
+            {
+                onlineUsers.Add(username, new List<string>{connectionId});
+            }
+        }
+
+        return Task.CompletedTask;
+    }
+
+    public Task UserDisconnected(string username, string connectionId)
+    {
+        lock (onlineUsers)
+        {
+            if (!onlineUsers.ContainsKey(username)) return Task.CompletedTask;
+
+            onlineUsers[username].Remove(connectionId);
+            if (onlineUsers[username].Count == 0)
+            {
+                onlineUsers.Remove(username);
+            }
+        }
+
+        return Task.CompletedTask;
+    }
+
+    public Task<string[]> GetOnlineUsers()
+    {
+        string[] users;
+
+        lock(onlineUsers)
+        {
+            users = onlineUsers.OrderBy(k => k.Key).Select(k => k.Key).ToArray();
+        }
+
+        return Task.FromResult(users);
+    }
+}
