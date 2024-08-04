511c99ca9a4fe9431f5ea854bc645060bdca41c0
Rob Blake <rob.bl8ke@outlook.com>
2024-08-04 11:30:15 +0200

    Send live messages between users in the SPA

--- client/src/app/_services/message.service.ts
+++ client/src/app/_services/message.service.ts
@@ -3,14 +3,51 @@ import { Injectable } from '@angular/core';
 import { environment } from 'src/environments/environment';
 import { Message } from '../_models/message';
 import { getPaginationHeaders, getPaginatedResult } from './paginationHelper';
+import { HubConnection, HubConnectionBuilder } from '@microsoft/signalr';
+import { User } from '../_models/user';
+import { BehaviorSubject, take } from 'rxjs';
 
 @Injectable({
     providedIn: 'root',
 })
 export class MessageService {
     baseUrl = environment.apiUrl;
+    hubUrl = environment.hubUrl;
+    private hubConnection?: HubConnection
+    private messageThreadSource = new BehaviorSubject<Message[]>([]);
+    messageThread$ = this.messageThreadSource.asObservable();
+
 
     constructor(private http: HttpClient) {}
+
+    createHubConnection(user: User, otherUsername: string) {
+        this.hubConnection = new HubConnectionBuilder()
+            .withUrl(this.hubUrl + 'message?user=' + otherUsername, {
+                accessTokenFactory: () => user.token
+            })
+            .withAutomaticReconnect()
+            .build();
+        
+        this.hubConnection.start().catch(error => console.log(error));
+
+        this.hubConnection.on('ReceiveMessageThread', messages => {
+            this.messageThreadSource.next(messages);
+        });
+
+        this.hubConnection.on('NewMessage', message => {
+            // Don't mutate the array. The spread operator will replace
+            // the array rather than mutate it (pushing/appending to it)
+            this.messageThread$.pipe(take(1)).subscribe({
+                next: messages => {
+                    this.messageThreadSource.next([...messages, message]);
+                }
+            });
+        });
+    }
+
+    stopHubConnection() {
+        if (this.hubConnection) this.hubConnection?.stop();
+    }
 
     getMessages(container: string, pageNumber: number, pageSize: number) {
         let params = getPaginationHeaders(pageNumber, pageSize);
@@ -23,9 +60,11 @@ export class MessageService {
         return this.http.get<Message[]>(this.baseUrl + 'messages/thread/' + username);
     }
 
-    sendMessage(username: string, content: string) {
-        // take a look at "CreateMessageDto"
-        return this.http.post<Message>(this.baseUrl + 'messages', {recipientUsername: username, content});
+    // async guarantees that we will get a promise from this method.
+    // although in this case this is what the invoke() says it will do!
+    async sendMessage(username: string, content: string) {
+        return this.hubConnection?.invoke('SendMessage', { recipientUsername: username, content })
+            .catch(error => console.log(error));
     }
 
     deleteMessage(id: number) {
--- client/src/app/members/member-detail/member-detail.component.ts
+++ client/src/app/members/member-detail/member-detail.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { Component, OnDestroy, OnInit, ViewChild } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { GalleryItem, GalleryModule, ImageItem } from 'ng-gallery';
 import { TabDirective, TabsModule, TabsetComponent } from 'ngx-bootstrap/tabs';
@@ -10,6 +10,9 @@ import { MemberMessagesComponent } from '../member-messages/member-messages.comp
 import { MessageService } from 'src/app/_services/message.service';
 import { Message } from 'src/app/_models/message';
 import { PresenceService } from 'src/app/_services/presence.service';
+import { AccountsService } from 'src/app/_services/accounts.service';
+import { take } from 'rxjs';
+import { User } from 'src/app/_models/user';
 
 @Component({
     standalone: true,
@@ -18,15 +21,23 @@ import { PresenceService } from 'src/app/_services/presence.service';
     styleUrls: ['./member-detail.component.css'],
     imports: [GalleryModule, TabsModule, CommonModule, TimeagoModule, MemberMessagesComponent]
 })
-export class MemberDetailComponent implements OnInit {
+export class MemberDetailComponent implements OnInit, OnDestroy {
     @ViewChild('memberTabs', { static: true }) memberTabs?: TabsetComponent;
     member: Member = {} as Member;
     images: GalleryItem[] = [];
     activeTab?: TabDirective
     messages: Message[] = [];
+    user?: User;
 
     constructor(private route: ActivatedRoute, private messageService: MessageService, 
-        public presenceService: PresenceService) { }
+        private accountService: AccountsService, public presenceService: PresenceService) {
+
+        this.accountService.currentUser$.pipe(take(1)).subscribe({
+            next: user => {
+                if (user) this.user = user;
+            }
+        });
+    }
 
     ngOnInit(): void {  
         this.route.data.subscribe({
@@ -40,6 +51,10 @@ export class MemberDetailComponent implements OnInit {
         this.getImages();
     }
 
+    ngOnDestroy(): void {
+        this.messageService.stopHubConnection();
+    }
+
     selectTab(heading: string) {
         if (this.memberTabs) {
             this.memberTabs.tabs.find(x => x.heading === heading)!.active = true;
@@ -48,8 +63,10 @@ export class MemberDetailComponent implements OnInit {
 
     onTabActivated(data: TabDirective) {
         this.activeTab = data;
-        if (this.activeTab.heading === 'Messages') {
-            this.loadMessages();
+        if (this.activeTab.heading === 'Messages' && this.user) {
+            this.messageService.createHubConnection(this.user, this.member.username);
+        } else {
+            this.messageService.stopHubConnection();
         }
     }
 
