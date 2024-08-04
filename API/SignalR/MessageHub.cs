cabfaa61b907c77da3d5dff0af2f1a5291de5a15
Rob Blake <rob.bl8ke@outlook.com>
2024-08-04 11:29:45 +0200

    Create and configure the message hub on the API

---
+++ API/SignalR/MessageHub.cs
@@ -0,0 +1,82 @@
+﻿using API.DTOs;
+using API.Entities;
+using API.Extensions;
+using API.Interfaces;
+using AutoMapper;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.SignalR;
+
+namespace API.SignalR;
+
+[Authorize]
+public class MessageHub : Hub
+{
+    private readonly IMessageRepository messageRepository;
+    private readonly IUserRepository userRepository;
+    private readonly IMapper mapper;
+
+    public MessageHub(IMessageRepository messageRepository, IUserRepository userRepository, 
+        IMapper mapper)
+    {
+        this.messageRepository = messageRepository;
+        this.userRepository = userRepository;
+        this.mapper = mapper;
+    }
+
+    public override async Task OnConnectedAsync()
+    {
+        var httpContext = Context.GetHttpContext();
+        var otherUser = httpContext.Request.Query["user"];
+        var groupName = GetGroupName(Context.User.GetUsername(), otherUser);
+
+        await Groups.AddToGroupAsync(Context.ConnectionId, groupName);
+
+        // On connection, and just having created a group, we'll get the message thread 
+        // (for this user pair) via SignalR. 
+        var messages = await messageRepository.GetMessageThread(Context.User.GetUsername(), otherUser);
+        await Clients.Group(groupName).SendAsync("ReceiveMessageThread", messages);
+    }
+
+    public override Task OnDisconnectedAsync(Exception exception)
+    {
+        // When a user disconnects from this hub by moving to another part of the application 
+        // they will be automatically removed from any groups they are connected to on this hub.
+        return base.OnDisconnectedAsync(exception);
+    }
+
+    public async Task SendMessage(CreateMessageDto createMessageDto)
+    {
+            var username = Context.User.GetUsername();
+
+            if (username == createMessageDto.RecipientUsername.ToLower())
+                throw new HubException("You cannot send messages to yourself");
+
+            var sender = await userRepository.GetUserByUsernameAsync(username);
+            var recipient = await userRepository.GetUserByUsernameAsync(createMessageDto.RecipientUsername);
+
+            if (recipient == null) throw new HubException("User not found");
+
+            var message = new Message
+            {
+                Sender = sender,
+                Recipient = recipient,
+                SenderUsername = sender.UserName,
+                RecipientUsername = recipient.UserName,
+                Content = createMessageDto.Content
+            };
+            
+            messageRepository.AddMessage(message);
+
+            if (await messageRepository.SaveAllAsync())
+            {
+                var group = GetGroupName(sender.UserName, recipient.UserName);
+                await Clients.Group(group).SendAsync("NewMessage", mapper.Map<MessageDto>(message));
+            }
+    }
+
+    private string GetGroupName(string caller, string other)
+    {
+        var stringCompare = string.CompareOrdinal(caller, other) < 0;
+        return stringCompare ? $"{caller}-{other}" : $"{other}-{caller}";
+    }
+}
